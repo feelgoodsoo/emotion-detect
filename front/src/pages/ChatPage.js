f070e6f99eabee3f3ccf2e8646f30ba8f7048ac5
PILSUCHOI <chlvlftn22@gmail.com>
2023-08-14 23:07:16 +0900

    feature/navbar add

---
+++ front/src/pages/ChatPage.js
@@ -0,0 +1,51 @@
+import "./ChatPage.css";
+import { useState } from "react";
+import { sendMessageToOpenAI } from "../api/gptRequest";
+
+function ChatPage() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState("");
+
+  //console.log("env name: ", process.env.REACT_APP_OPENAI_API_KEY);
+
+  const handleMessageSubmit = async () => {
+    const response = await sendMessageToOpenAI(input);
+    setMessages([
+      ...messages,
+      { text: input, isUser: true },
+      { text: response, isUser: false },
+    ]);
+    setInput("");
+  };
+
+  return (
+    <div className="page_Container">
+      <div className="chat">
+        {Array.isArray(messages)
+          ? messages.map((message, index) => (
+              <div
+                key={index}
+                className={message.isUser ? "user-message" : "bot-message"}
+              >
+                {message.text}
+              </div>
+            ))
+          : null}
+      </div>
+
+      <div className="input-container">
+        <input
+          className="input"
+          tpye="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="button" onClick={handleMessageSubmit}>
+          Send
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ChatPage;
